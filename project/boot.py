ee1bc7352efef23f15e7d6d346fb8af97d06715a
poqob <mmermmus123@gmail.com>
2025-04-07 01:44:16 +0300

    main loop tested. (success)

--- project/boot.py
+++ project/boot.py
@@ -1,12 +1,10 @@
-from home.main import main
+from home.main import main, once
 from home.setup import Setup
 
-def loop():
-    pass
-
 if __name__=='__main__':
-    setup = Setup(callback=main, config_path="/config.json",loop=loop)
+    setup = Setup(callback=once, config_path="/config.json",loop=main)
     setup.setup_all()
     setup.run_callback()
+    setup.run_main_loop()
     
     
--- project/home/main.py
+++ project/home/main.py
@@ -1,6 +1,20 @@
 # main.py
 from micropython import const
+import time
+from machine import Pin
+
 MANIFEST = const('I am Dağ, a Turkish guy who loves her country 🇹🇷.')
-def main():
+
+def once():
     print(MANIFEST)
+    
+
+led = Pin(2, Pin.OUT)
+
+def main():
+    led.value(1)
+    time.sleep(0.25)
+    led.value(0)
+    time.sleep(0.25)
+    
     
